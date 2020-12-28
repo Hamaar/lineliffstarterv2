6f61671890bb35ebfc3e22969e7c2929bb6e746a
hafis mandala <hfis15@yahoo.co.id>
2020-12-28 10:44:14 +0700

    add line id

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
-const myLiffId = process.env.MY_LIFF_ID;
+const myLiffId = "1655450021-1XgpRMEA";
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/send-id', function(req, res) {
-    res.json({id: myLiffId});
+app.get("/send-id", function (req, res) {
+  res.json({ id: myLiffId });
 });
 
 app.listen(port, () => console.log(`app listening on port ${port}!`));
